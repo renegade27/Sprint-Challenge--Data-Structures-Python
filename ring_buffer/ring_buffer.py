df981dce95a42b9050eebbe4476cfdbab0aa03f6
renegade27 <foemaster@live.com>
2019-09-20 13:02:55 -0500

    MVP complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,20 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if None not in self.storage:
+      if self.current >= self.capacity-1:
+        self.current = 0
+      self.storage[self.current] = item
+      self.current += 1
+      return
+    self.storage[self.current] = item
+    self.current += 1
+    return
+    
 
   def get(self):
-    pass
+    max = self.capacity
+    for i in range(self.capacity):
+      if self.storage[i-1] == None:
+        max = i-1
+    return self.storage[:max]
